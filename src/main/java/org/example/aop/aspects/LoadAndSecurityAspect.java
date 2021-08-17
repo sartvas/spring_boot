b121eccb1f02e7ed169649a3dd85ab5e030c717e
Egor <sartvas@yandex.ru>
2021-08-17 17:48:08 +0300

    AOP: Aspect` methods. Advices. LiveHacks whith pointats declaration.

--- src/main/java/org/example/aop/aspects/LoadAndSecurityAspect.java
+++ src/main/java/org/example/aop/aspects/LoadAndSecurityAspect.java
@@ -3,13 +3,17 @@ package org.example.aop.aspects;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoadAndSecurityAspect {
 
-    @Before("execution(* get*())")
+    @Pointcut("execution(* get*())")
+    private void allGetMethods(){}
+
+    @Before("allGetMethods()")
     // public void getBook(org.example.aop.Book, ..) - если параметр - это класс, пишем весь путь и .. или см. ниже
     // public void *(*) - 1 любое имя с любым одним пареметром
     // public void *(..) - 1 любое имя с любым количеством пареметров
@@ -17,13 +21,15 @@ public class LoadAndSecurityAspect {
         System.out.println("Somebody trying to take a book/magazine from library");
     }
 
+    @Before("allGetMethods()")
+    public void beforeGetSecurityAdvice(){
+        System.out.println("Check security");
+    }
+
     @Before("execution(* returnBook())")
     public void beforeReturnBookAdvice(){
         System.out.println("Somebody trying to return a book to library");
     }
 
-    @Before("execution(* get*())")
-    public void beforeGetSecurityAdvice(){
-        System.out.println("Check security");
-    }
+
 }
